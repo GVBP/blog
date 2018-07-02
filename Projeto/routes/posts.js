7da3e0700c976fc5efe06f19b4406ae1c8ed6a12
GUILHERME VICTOR BORGES PEREIRA <pereira.guilherme@totvs.com.br>
2018-07-02 17:39:22 -0300

    Nova Alteração 02/07/2018

--- Projeto/routes/posts.js
+++ Projeto/routes/posts.js
@@ -2,24 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 // Carrega/Executa trazendo apenas o retorno da function
-var model = require('./../model/posts')();
+var modelPost = require('./../model/posts')();
+var modelUser = require('./../model/users')();
+var modelComments = require('./../model/comments')();
+
+var json = {};
 
 
 /* GET posts page */
 // next referece a próxima middleware
 router.get('/', function (req, res, next) {
 
-    model.find(null, function (err, posts) {
+    modelPost.find(null, function (err, posts) {
+        // if (err) throw err;
+        // res.render('posts', { title: 'Postagens', posts: posts });
+        json.posts = posts;
 
-        if (err) throw err;
-        res.render('posts', { title: 'Postagens', posts: posts });
+        modelUser.find(null, function(err, users) {
+            json.users = users;
+
+            modelComments.find(null, function(err, comments) {
+                json.comments = comments;
+
+                res.json(json);
+
+                // res.write().json(json);
+            });
+        });
     });
 });
 
 /* GET posts/:id -> comments */
 router.get('/:id', function (req, res) {
     var id = req.params.id;
-    model.findOne({ "_id" : id }, (err, doc) => {
+    modelPost.findOne({ "_id" : id }, (err, doc) => {
         
         if (err) throw err;
         res.render('comments', { title: "Comentários", doc });
